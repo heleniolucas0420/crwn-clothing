1db13796cbc607bfc7033224bf39e5f186ba2454
CitedWhistle <heleniolucas07@gmail.com>
2021-06-19 11:16:59 +0200

    added redux, and implemented userReducer and userActions to header and app components

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -31,6 +31,7 @@ class SignUp extends React.Component {
 
     try {
       const { user } = await auth.createUserWithEmailAndPassword(email, password);
+      console.log(user);
 
       await createUserProfileDocument(user, { displayName });
 
