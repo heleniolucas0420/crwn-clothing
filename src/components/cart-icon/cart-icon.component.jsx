494ab6e35a1d7c9e0eaa6e80b2086f554075160c
CitedWhistle <heleniolucas07@gmail.com>
2021-06-25 17:55:54 +0200

    updating cart-icon component to the right selector

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
-import { selectCartItems, selectCartItemsCount } from '../../redux/cart/cart.selectors';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
@@ -17,7 +17,7 @@ const CartIcon = ({ toggleCartHidden, itemCount }) => (
 
 
 const mapStateToProps = state => ({
-  itemCount: selectCartItems(state)
+  itemCount: selectCartItemsCount(state)
 });
 
 const mapDispatchToProps = dispatch => ({
